75deb91384d718d984493e11f16699e5f9f65a25
STJANG <termblur@gmail.com>
2024-02-23 15:44:14 +0900

    [chore] urls

--- BoxOffice/Helper/Constants.swift
+++ BoxOffice/Helper/Constants.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+enum BoxOffice {
+    static let weeklyBoxOfficeListUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json"
+    static let movieInfoUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+}
