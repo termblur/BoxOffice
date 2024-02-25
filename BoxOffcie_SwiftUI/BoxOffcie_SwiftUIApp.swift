a93f814ef99884d9e005ca44d9d0ff1de17692fc
termblur <termblur@gmail.com>
2024-02-25 16:00:23 +0900

    [init] SwiftUI

---
+++ BoxOffcie_SwiftUI/BoxOffcie_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOffcie_SwiftUIApp.swift
+//  BoxOffcie_SwiftUI
+//
+//  Created by termblur on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxOffcie_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
